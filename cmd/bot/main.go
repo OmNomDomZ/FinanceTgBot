292b584fc7bb174397a9f70c797aa8129494369d
omnomdom <v.rabetskii@gmail.com>
2024-10-28 23:26:10 +0700

    add: add truncate data func

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -16,8 +16,8 @@ import (
 )
 
 type UserState struct {
-	AwaitingAmount  bool
-	TransactionType string // "доход" или "расход"
+	awaitingAmount  bool
+	transactionType string // "доход", "расход", "удаление"
 }
 
 var userStates = make(map[int64]*UserState)
@@ -29,8 +29,12 @@ func sendKeyboardButton(bot *telego.Bot, update telego.Update) {
 		tu.KeyboardRow(
 			tu.KeyboardButton("Добавить доход"),
 			tu.KeyboardButton("Добавить расход"),
+		),
+		tu.KeyboardRow(
 			tu.KeyboardButton("Вывести все транзакции"),
-		))
+			tu.KeyboardButton("Стереть все транзакции"),
+		),
+	)
 
 	message := tu.Message(
 		chatID,
@@ -44,7 +48,7 @@ func handleAddTransaction(bot *telego.Bot, update telego.Update, db *sql.DB) {
 	chatID := update.Message.Chat.ID
 
 	state, exists := userStates[chatID]
-	if exists && state.AwaitingAmount {
+	if exists && state.awaitingAmount {
 
 		amount, err := strconv.ParseFloat(update.Message.Text, 64)
 		if err != nil {
@@ -56,7 +60,7 @@ func handleAddTransaction(bot *telego.Bot, update telego.Update, db *sql.DB) {
 			return
 		}
 
-		_, err = db.Exec("INSERT INTO transactions (amount, transaction_type) VALUES ($1, $2)", amount, state.TransactionType)
+		_, err = db.Exec("INSERT INTO transactions (amount, transaction_type) VALUES ($1, $2)", amount, state.transactionType)
 		if err != nil {
 			message := tu.Message(
 				tu.ID(chatID),
@@ -68,7 +72,7 @@ func handleAddTransaction(bot *telego.Bot, update telego.Update, db *sql.DB) {
 
 		message := tu.Message(
 			tu.ID(chatID),
-			fmt.Sprintf("%s в размере %.2f успешно добавлен!", state.TransactionType, amount),
+			fmt.Sprintf("%s в размере %.2f успешно добавлен!", state.transactionType, amount),
 		)
 		bot.SendMessage(message)
 
@@ -80,8 +84,8 @@ func handleIncomeCommand(bot *telego.Bot, update telego.Update) {
 	chatID := update.Message.Chat.ID
 
 	userStates[chatID] = &UserState{
-		AwaitingAmount:  true,
-		TransactionType: "доход",
+		awaitingAmount:  true,
+		transactionType: "доход",
 	}
 
 	message := tu.Message(
@@ -96,8 +100,8 @@ func handleExpenseCommand(bot *telego.Bot, update telego.Update) {
 	chatID := update.Message.Chat.ID
 
 	userStates[chatID] = &UserState{
-		AwaitingAmount:  true,
-		TransactionType: "расход",
+		awaitingAmount:  true,
+		transactionType: "расход",
 	}
 
 	message := tu.Message(
@@ -108,7 +112,7 @@ func handleExpenseCommand(bot *telego.Bot, update telego.Update) {
 	bot.SendMessage(message)
 }
 
-func handleMonthlyInfo(bot *telego.Bot, update telego.Update, db *sql.DB) {
+func handleTransactionsInfo(bot *telego.Bot, update telego.Update, db *sql.DB) {
 	chatID := tu.ID(update.Message.Chat.ID)
 
 	message := tu.Message(
@@ -130,6 +134,7 @@ func handleMonthlyInfo(bot *telego.Bot, update telego.Update, db *sql.DB) {
 	defer rows.Close()
 
 	for rows.Next() {
+		index := 1
 		var id int
 		var amount float64
 		var transactionType string
@@ -140,11 +145,70 @@ func handleMonthlyInfo(bot *telego.Bot, update telego.Update, db *sql.DB) {
 		}
 		message := tu.Message(
 			chatID,
-			fmt.Sprintf("id: %d, amount: %.2f, transaction_type: %s, created_at: %s", id, amount, transactionType, createdAt.Format("02-01-2006 15:04:05")),
+			fmt.Sprintf("%d: Amount: %.2f\n Transaction_type: %s\n Created_at: %s", index, amount, transactionType, createdAt.Format("02-01-2006 15:04:05")),
 		)
 		bot.SendMessage(message)
+		index++
+	}
+}
+
+func handleClearTable(bot *telego.Bot, update telego.Update) {
+	chatID := update.Message.Chat.ID
+
+	userStates[chatID] = &UserState{
+		awaitingAmount:  false,
+		transactionType: "удаление",
 	}
 
+	message := tu.Message(
+		tu.ID(chatID),
+		"Вы уверены, что хотите стереть все данные?\n[Да / Нет]",
+	)
+	bot.SendMessage(message)
+}
+
+func handleConfirmation(bot *telego.Bot, update telego.Update, db *sql.DB) {
+	chatID := update.Message.Chat.ID
+
+	state, exists := userStates[chatID]
+	if !exists || state.transactionType != "удаление" {
+		return
+	}
+	agreement := update.Message.Text
+
+	if agreement == "Да" {
+		_, err := db.Exec("TRUNCATE TABLE transactions RESTART IDENTITY;\n")
+		if err != nil {
+			log.Printf("Ошибка при удалении данных: %v", err)
+			message := tu.Message(
+				tu.ID(chatID),
+				"Произошла ошибка при удалении данных. Попробуйте позже.",
+			)
+			bot.SendMessage(message)
+			return
+		}
+
+		message := tu.Message(
+			tu.ID(chatID),
+			"Все транзакции успешно стерты.",
+		)
+		bot.SendMessage(message)
+	} else if agreement == "Нет" {
+		message := tu.Message(
+			tu.ID(chatID),
+			"Удаление данных отменено.",
+		)
+		bot.SendMessage(message)
+	} else {
+		message := tu.Message(
+			tu.ID(chatID),
+			"Пожалуйста, ответьте 'Да' или 'Нет'.",
+		)
+		bot.SendMessage(message)
+		return
+	}
+
+	delete(userStates, chatID)
 }
 
 func main() {
@@ -171,7 +235,6 @@ func main() {
 	defer bot.StopLongPolling()
 
 	bh.Handle(sendKeyboardButton, th.CommandEqual("start"))
-	//bh.Handle(sendAnyMessage, th.AnyMessage())
 
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
 		handleIncomeCommand(bot, update)
@@ -182,11 +245,32 @@ func main() {
 	}, th.TextEqual("Добавить расход"))
 
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		handleMonthlyInfo(bot, update, db)
+		handleTransactionsInfo(bot, update, db)
 	}, th.TextEqual("Вывести все транзакции"))
 
 	bh.Handle(func(bot *telego.Bot, update telego.Update) {
-		handleAddTransaction(bot, update, db)
+		handleClearTable(bot, update)
+	}, th.TextEqual("Стереть все транзакции"))
+
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		chatID := update.Message.Chat.ID
+
+		state, exists := userStates[chatID]
+		if !exists {
+			return
+		}
+
+		if state.transactionType == "удаление" {
+			handleConfirmation(bot, update, db)
+			return
+		}
+
+		if state.awaitingAmount {
+			handleAddTransaction(bot, update, db)
+			return
+		}
+
+		sendKeyboardButton(bot, update)
 	}, th.AnyMessage())
 
 	bh.Start()
